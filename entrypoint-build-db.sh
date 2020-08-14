b489aea625b383876b6e43f567cd3eec9a9226f6
minketeer <minying@gmail.com>
2020-08-14 15:24:15 +0100

    initial docker compose builds

---
+++ entrypoint-build-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# prisma schema migration and data seeding
+yarn -s prisma migrate save --experimental
+yarn -s prisma migrate up --experimental
+yarn -s prisma generate
+yarn seed
+
+yarn start:rest
